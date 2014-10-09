e1a16c3a52d5ec0f43b0ce638f2c8206550369a8
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2014-10-09 10:02:22 +0300

    Removed HomePageController and its viewer only to be managed by receiving implementing module(kenyaEMR or openMRS2.x)

--- omod/src/main/java/org/openmrs/module/openconceptlab/extension/html/AdminList.java
+++ omod/src/main/java/org/openmrs/module/openconceptlab/extension/html/AdminList.java
@@ -44,7 +44,7 @@ public class AdminList extends AdministrationSectionExt {
 	 */
 	public Map<String, String> getLinks() {
 		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
-		map.put("/openconceptlab/home.page", "openconceptlab.configure");
+		map.put("/openconceptlab/configure.page", "openconceptlab.configure");
 		return map;
 	}
 	
