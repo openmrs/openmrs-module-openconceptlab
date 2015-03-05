375556b0a93dd4cd04f7dfd17c67c70ac1885877
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2015-03-05 18:26:03 +0300

    Replace finding the size of a list with criteria

--- omod/src/main/java/org/openmrs/module/openconceptlab/fragment/controller/status/NextUpdatesFragmentController.java
+++ omod/src/main/java/org/openmrs/module/openconceptlab/fragment/controller/status/NextUpdatesFragmentController.java
@@ -13,7 +13,6 @@
  */
 package org.openmrs.module.openconceptlab.fragment.controller.status;
 
-import org.openmrs.module.openconceptlab.Item;
 import org.openmrs.module.openconceptlab.State;
 import org.openmrs.module.openconceptlab.Subscription;
 import org.openmrs.module.openconceptlab.Update;
@@ -22,9 +21,8 @@ import org.openmrs.module.openconceptlab.Utils;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -33,19 +31,15 @@ import java.util.Set;
 public class NextUpdatesFragmentController {
 	
 	public void controller(FragmentModel model, @SpringBean UpdateService service) {
+
 		Update lastUpdate = service.getLastUpdate();
-		Set<Item> updatedItems;
-		List<Item> errorItems = new ArrayList<Item>();
+
+		Set<State> states = new HashSet<State>();
+		states.add(State.ERROR);
+		int errorsItems = 0;
 		if (lastUpdate != null) {
 			model.addAttribute("lastUpdateId", lastUpdate.getUpdateId());
-			
-			updatedItems = lastUpdate.getItems();
-			errorItems = new ArrayList<Item>();
-			for (Item item : updatedItems) {
-				if (item.getState().equals(State.ERROR)) {
-					errorItems.add(item);
-				}
-			}
+			errorsItems = service.getUpdateItemsCount(lastUpdate, states);
 		}
 		
 		Date lastUpdateDate;
@@ -74,7 +68,7 @@ public class NextUpdatesFragmentController {
 		
 		model.addAttribute("nextUpdateDate", Utils.formatedDate(Utils.dateAddDays(lastUpdateDate, days)));
 		model.addAttribute("nextUpdateTime", appendZeros(hours.toString()) + ":" + appendZeros(minutes.toString()));
-		model.addAttribute("errorItemSize", errorItems.size());
+		model.addAttribute("errorItemSize", errorsItems);
 		model.addAttribute("manual", manual);
 		
 	}
