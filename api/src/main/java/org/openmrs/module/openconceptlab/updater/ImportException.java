750b2e7a2998a3292ddebb9dbeaf4c92bb35f9bd
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-07-29 13:41:06 +0200

    Fixed compilation error for jdk6

--- api/src/main/java/org/openmrs/module/openconceptlab/updater/ImportException.java
+++ api/src/main/java/org/openmrs/module/openconceptlab/updater/ImportException.java
@@ -8,10 +8,6 @@ public class ImportException extends RuntimeException {
 		super();
 	}
 	
-	public ImportException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-	
 	public ImportException(String message, Throwable cause) {
 		super(message, cause);
 	}
