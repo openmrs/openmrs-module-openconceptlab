f911c299021737b21b2e0fa62178c3bdd1aa0a20
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2017-01-17 16:15:07 +0100

    Running import in the startup thread

--- api/src/main/java/org/openmrs/module/openconceptlab/OpenConceptLabActivator.java
+++ api/src/main/java/org/openmrs/module/openconceptlab/OpenConceptLabActivator.java
@@ -79,8 +79,7 @@ public class OpenConceptLabActivator implements ModuleActivator, DaemonTokenAwar
 				try {
 					ZipFile zipFile = (new ZipFile(files[0]));
 					Importer importer = Context.getRegisteredComponent("openconceptlab.importer", Importer.class);
-					importer.setZipFile(zipFile);
-					scheduler.scheduleNow();
+					importer.run(zipFile);
 				} catch (IOException e) {
 					throw new IllegalStateException("Failed to open zip file", e);
 				}
