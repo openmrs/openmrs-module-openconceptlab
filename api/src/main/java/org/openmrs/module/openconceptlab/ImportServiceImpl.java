6f55e03a1d27a48b20e9bb76a9044101c30a7150
Ian <52504170+ibacher@users.noreply.github.com>
2026-04-02 18:06:35 -0400

    OCLOMRS-1122: Decode any HTML-encoding of the key (#124)

--- api/src/main/java/org/openmrs/module/openconceptlab/ImportServiceImpl.java
+++ api/src/main/java/org/openmrs/module/openconceptlab/ImportServiceImpl.java
@@ -9,6 +9,7 @@
  */
 package org.openmrs.module.openconceptlab;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Criteria;
 import org.hibernate.Query;
@@ -359,13 +360,13 @@ public class ImportServiceImpl implements ImportService {
 			return null;
 		}
 		Subscription subscription = new Subscription();
-		subscription.setUrl(url);
+		subscription.setUrl(StringEscapeUtils.unescapeHtml4(url));
 
 		String uuid = adminService.getGlobalProperty(OpenConceptLabConstants.GP_SUBSCRIPTION_UUID);
 		subscription.setUuid(uuid);
 
 		String token = adminService.getGlobalProperty(OpenConceptLabConstants.GP_TOKEN);
-		subscription.setToken(token);
+		subscription.setToken(StringEscapeUtils.unescapeHtml4(token));
 
 		String validationType = adminService.getGlobalProperty(OpenConceptLabConstants.GP_VALIDATION_TYPE);
 		if (StringUtils.isNotBlank(validationType)) {
